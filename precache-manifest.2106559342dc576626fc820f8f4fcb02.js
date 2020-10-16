576560b64fa9488d40f4d52fb0d9c61963b2befc
adityaiiestian@gmail.com <adityaiiestian@gmail.com>
2020-10-16 16:04:02 +0530

    Updates

---
+++ precache-manifest.2106559342dc576626fc820f8f4fcb02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d68912d3e5aadb0ae0724a57bc66f5",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "1ba091aa6c8fe55d1546",
+    "url": "/myapp/static/css/main.c9c72ace.chunk.css"
+  },
+  {
+    "revision": "44557d959d96510cf5fb",
+    "url": "/myapp/static/js/2.2357ea90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.2357ea90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ba091aa6c8fe55d1546",
+    "url": "/myapp/static/js/main.ee48dd29.chunk.js"
+  },
+  {
+    "revision": "c15ec7c7f0e65d57e2c1",
+    "url": "/myapp/static/js/runtime-main.950194d4.js"
+  }
+]);
